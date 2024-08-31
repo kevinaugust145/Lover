e718722397cabf61f93664d997d98bb27e862898
KevinHung <kevinaugust145@gmail.com>
2024-08-31 22:22:30 +0800

    Initial Commit

---
+++ LoverTests/LoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoverTests.swift
+//  LoverTests
+//
+//  Created by 洪宗燦 on 2024/8/31.
+//
+
+import Testing
+@testable import Lover
+
+struct LoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
